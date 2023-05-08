33ca5cf5f65003207428200e88d4c2ffd430b0d8
Akar Jaza <akarslemany08@gmail.com>
2023-05-08 21:57:06 +0300

    card extracted

--- lib/Homepage.dart
+++ lib/Homepage.dart
@@ -34,17 +34,47 @@ class _HomePageState extends State<HomePage> {
         body: Center(
           child: Column(
             children: [
-              Container(
-                margin:
-                    const EdgeInsets.symmetric(vertical: 35, horizontal: 30),
-                height: 140,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: const Color(0xFF5FCCA0),
-                ),
+              Row(
+                children: [
+                  Card(),
+                ],
               )
             ],
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class Card extends StatelessWidget {
+  const Card({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerRight,
+      margin:
+          const EdgeInsets.symmetric(vertical: 35, horizontal: 60),
+      height: 80,
+      width: 150,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: const Color(0xFF5FCCA0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/list.png',
+              height: 20,
+              color: Colors.black,
+            ),
+            const Text("ڕیزبەندی"),
+          ],
         ),
       ),
     );
