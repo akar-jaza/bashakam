8c80612d3bfea68d64f7f5430b424a07967bfa3f
Akar Jaza <akarslemany08@gmail.com>
2023-05-21 23:33:40 +0300

    .

--- lib/components/textfieldView.dart
+++ lib/components/textfieldView.dart
@@ -14,22 +14,28 @@ class TextFieldView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      controller: _textController,
-      cursorColor: colorFile.kblueColor,
-      style: TextStyle(color: colorFile.kWhiteTextColor),
-      decoration: InputDecoration(
-        labelText: 'وانە',
-        contentPadding:
-            const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-        enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: colorFile.kMyCardColor),
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10),
+      child: TextField(
+        controller: _textController,
+        cursorColor: colorFile.kblueColor,
+        style: TextStyle(
+          color: colorFile.kWhiteTextColor,
+          fontFamily: 'Roboto',
         ),
-        focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: colorFile.kMyCardColor),
+        decoration: InputDecoration(
+          labelText: 'وانە',
+          contentPadding:
+              const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: colorFile.kMyCardColor),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: colorFile.kMyCardColor),
+          ),
+          fillColor: colorFile.kMyCardColor,
+          filled: true,
         ),
-        fillColor: colorFile.kMyCardColor,
-        filled: true,
       ),
     );
   }
