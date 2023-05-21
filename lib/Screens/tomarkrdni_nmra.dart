617c23b557d82baed658717d2bd8c27f82ba99fa
Akar Jaza <akarslemany08@gmail.com>
2023-05-22 00:06:11 +0300

    .

--- lib/Screens/tomarkrdni_nmra.dart
+++ lib/Screens/tomarkrdni_nmra.dart
@@ -129,6 +129,31 @@ class _TomarkrdniNmraPageState extends State<TomarkrdniNmraPage> {
                 ),
               ],
             ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 170,
+                  child: TextFieldView(
+                    textController: _textController,
+                    colorFile: colorFile,
+                    labelText: 'عەرەبی',
+                  ),
+                ),
+                SizedBox(width: 20),
+                SizedBox(
+                  width: 170,
+                  child: TextFieldView(
+                    textController: _textController2,
+                    colorFile: colorFile,
+                    labelText: 'کوردی',
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
