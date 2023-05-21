87865ca297e9a18cd55cf833bc3fb145ec0ebb2b
Akar Jaza <akarslemany08@gmail.com>
2023-05-21 23:55:00 +0300

    fixes

--- lib/components/textfieldView.dart
+++ lib/components/textfieldView.dart
@@ -17,40 +17,33 @@ class TextFieldView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: 10),
-      child: Theme(
-        data: ThemeData(
-            textSelectionTheme: TextSelectionThemeData(
-                cursorColor: colorFile.kblueColor, // Set the cursor color
-                selectionColor: colorFile.kWhiteTextColor,
-                selectionHandleColor: colorFile.kWhiteTextColor,
-                )),
-        child: TextField(
-          controller: _textController,
-          cursorColor: colorFile.kblueColor,
-          style: TextStyle(
-            color: colorFile.kWhiteTextColor,
-            fontFamily: 'Roboto',
-          ),
-          decoration: InputDecoration(
-            labelText: 'وانە',
-            contentPadding:
-                const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: colorFile.kMyCardColor),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: colorFile.kMyCardColor),
-            ),
-            fillColor: colorFile.kMyCardColor,
-            filled: true,
-          ),
-          enableInteractiveSelection: true,
-          keyboardType: TextInputType.number,
-          inputFormatters: <TextInputFormatter>[
-            FilteringTextInputFormatter
-                .digitsOnly // Restrict input to digits only
-          ],
+      child: TextField(
+        controller: _textController,
+        cursorColor: colorFile.kblueColor,
+        style: TextStyle(
+          color: colorFile.kWhiteTextColor,
+          fontFamily: 'Roboto',
         ),
+        decoration: InputDecoration(
+          labelText: 'وانە',
+          labelStyle: TextStyle(color: colorFile.kblueColor),
+          contentPadding:
+              const EdgeInsets.symmetric(vertical: 15, horizontal: 10),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: colorFile.kMyCardColor),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: colorFile.kMyCardColor),
+          ),
+          fillColor: colorFile.kMyCardColor,
+          filled: true,
+        ),
+        enableInteractiveSelection: true,
+        keyboardType: TextInputType.number,
+        inputFormatters: <TextInputFormatter>[
+          FilteringTextInputFormatter
+              .digitsOnly // Restrict input to digits only
+        ],
       ),
     );
   }
