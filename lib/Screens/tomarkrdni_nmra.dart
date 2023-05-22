bb2bf1bdcaa55d28b23b68fc219a593600ced11d
Akar Jaza <akarslemany08@gmail.com>
2023-05-22 22:01:20 +0300

    .

--- lib/Screens/tomarkrdni_nmra.dart
+++ lib/Screens/tomarkrdni_nmra.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:bashakam_barawzanko/components/my_button.dart';
 import 'package:bashakam_barawzanko/constantes/Colors.dart';
 import 'package:flutter/material.dart';
 
@@ -149,6 +150,10 @@ class _TomarkrdniNmraPageState extends State<TomarkrdniNmraPage> {
                 ),
               ],
             ),
+            SizedBox(
+              height: 20,
+            ),
+            MyButton()
           ],
         ),
       ),
---
+++ lib/components/my_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatefulWidget {
+  const MyButton({super.key});
+
+  @override
+  State<MyButton> createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton> {
+  @override
+  Widget build(BuildContext context) {
+    return FilledButton(
+      onPressed: () {},
+      child: Text('تۆمار بکە'),
+    );
+  }
+}
