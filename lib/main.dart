6837bfd29f72024b0629a30d5632fa1792d72d92
Akar Jaza <akarslemany08@gmail.com>
2023-05-25 23:54:36 +0300

    renaming and chaning some codes in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,15 @@
-// ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'Screens/home_page.dart';
-import 'constantes/colors.dart';
 import 'constantes/system_ui_overlay_func.dart';
 
 void main() {
   SystemUiOverlayFunc uiOverlayFunc = SystemUiOverlayFunc();
   uiOverlayFunc.systemUiOverlay();
-  runApp(Main());
+  runApp(MyApp());
 }
 
 // ignore: use_key_in_widget_constructors
-class Main extends StatelessWidget {
-  final ThemeColors colorFile = ThemeColors();
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,9 +17,8 @@ class Main extends StatelessWidget {
       theme: ThemeData(
         useMaterial3: true,
         fontFamily: 'rabarBold',
-        
       ),
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
