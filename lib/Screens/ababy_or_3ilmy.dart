7d54546783b4d19b319610b52d48bf2a81cbe5c5
Akar Jaza <akarslemany08@gmail.com>
2023-05-25 22:58:44 +0300

    .

--- lib/Screens/ababy_or_3ilmy.dart
+++ lib/Screens/ababy_or_3ilmy.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 import 'package:bashakam_barawzanko/Screens/nmrakanm.dart';
-import 'package:bashakam_barawzanko/Screens/tomarkrdni_nmra.dart';
+import 'package:bashakam_barawzanko/Screens/tomarkrdni_nmray_zansty.dart';
 import 'package:bashakam_barawzanko/constantes/Colors.dart';
 import 'package:flutter/material.dart';
 
