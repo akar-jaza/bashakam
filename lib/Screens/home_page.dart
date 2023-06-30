eb2c65bfa8d6f6beca2dd1d1423f2843f40d60cc
Akar Jaza <akarslemany08@gmail.com>
2023-07-01 00:19:03 +0300

    removed safeArea widget

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -23,138 +23,136 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
+    return Scaffold(
+      backgroundColor: ThemeColors.kBodyColor,
+      appBar: AppBar(
         backgroundColor: ThemeColors.kBodyColor,
-        appBar: AppBar(
-          backgroundColor: ThemeColors.kBodyColor,
-          title: const Center(
-            child: Text(
-              '🎓 بەشەکەم',
-              style: TextStyle(
-                fontSize: 18,
-                color: ThemeColors.kWhiteTextColor,
-                fontFamily: 'rabarBold',
-              ),
+        title: const Center(
+          child: Text(
+            '🎓 بەشەکەم',
+            style: TextStyle(
+              fontSize: 18,
+              color: ThemeColors.kWhiteTextColor,
+              fontFamily: 'rabarBold',
             ),
           ),
-          leading: Builder(
-            builder: (BuildContext context) {
-              return IconButton(
-                color: Colors.white,
-                icon: const Icon(
-                  Icons.menu_outlined,
-                  size: 18,
-                ),
-                onPressed: () {
-                  Scaffold.of(context).openDrawer();
-                },
-                tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-              );
-            },
-          ),
-          actions: [
-            IconButton(
-              onPressed: () {},
-              icon: IconButton(
-                onPressed: () {},
-                icon: ClipRRect(
-                  borderRadius: BorderRadius.circular(12.0),
-                  child: Image.asset(
-                    'assets/images/flo.png',
-                  ),
-                ),
-              ),
-              color: Colors.white,
-            ),
-          ],
         ),
-        body: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: Center(
-            child: Column(
-              children: [
-                const SizedBox(
-                  height: 25,
+        leading: Builder(
+          builder: (BuildContext context) {
+            return IconButton(
+              color: Colors.white,
+              icon: const Icon(
+                Icons.menu_outlined,
+                size: 18,
+              ),
+              onPressed: () {
+                Scaffold.of(context).openDrawer();
+              },
+              tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
+            );
+          },
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: IconButton(
+              onPressed: () {},
+              icon: ClipRRect(
+                borderRadius: BorderRadius.circular(12.0),
+                child: Image.asset(
+                  'assets/images/flo.png',
                 ),
-                SvgPicture.asset(
-                  'assets/images/cats.svg',
-                  width: 200,
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                const Text(
-                  'ڕیزبەندی بکە لەگەڵ ئەپڵیکەیشنی',
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: ThemeColors.kWhiteTextColor,
-                    fontFamily: 'rabarBold',
-                  ),
-                ),
-                const Text(
-                  '🎓 بەشەکەم',
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: ThemeColors.kWhiteTextColor,
-                    fontFamily: 'rabarBold',
-                  ),
-                ),
-                const SizedBox(height: 15),
-                const Text(
-                  'ڕیزبەندی بکە، ڕیزبەندیەکانت ببینە، زانیاری لەسەر بەشەکان ببینە',
-                  style: TextStyle(
-                    color: ThemeColors.kGreyTextColor,
-                    fontSize: 12,
-                  ),
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    MyCard(
-                      imageAsset: 'assets/images/list3.svg',
-                      buttonTitle: 'ببینە',
-                      color: ThemeColors.kWhiteTextColor,
-                      text: 'کەمترین کۆنمرە',
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const KamtrinKonmra()),
-                        );
-                      },
-                    ),
-                    MyCard(
-                      imageAsset: 'assets/images/id.svg',
-                      buttonTitle: 'ببینە',
-                      color: ThemeColors.kWhiteTextColor,
-                      text: 'نمرەکانم',
-                      onTap: () {},
-                    ),
-                  ],
-                ),
-                const Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    MyCard(
-                      imageAsset: 'assets/images/zarabin.svg',
-                      buttonTitle: 'ببینە',
-                      color: ThemeColors.kWhiteTextColor,
-                      text: 'ڕیزبەندیەکانم',
-                    ),
-                    MyCard(
-                      imageAsset: 'assets/images/departments.svg',
-                      buttonTitle: 'ببینە',
-                      color: ThemeColors.kWhiteTextColor,
-                      text: 'بەشەکان',
-                    ),
-                  ],
-                ),
-              ],
+              ),
             ),
+            color: Colors.white,
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 25,
+              ),
+              SvgPicture.asset(
+                'assets/images/cats.svg',
+                width: 200,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text(
+                'ڕیزبەندی بکە لەگەڵ ئەپڵیکەیشنی',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: ThemeColors.kWhiteTextColor,
+                  fontFamily: 'rabarBold',
+                ),
+              ),
+              const Text(
+                '🎓 بەشەکەم',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: ThemeColors.kWhiteTextColor,
+                  fontFamily: 'rabarBold',
+                ),
+              ),
+              const SizedBox(height: 15),
+              const Text(
+                'ڕیزبەندی بکە، ڕیزبەندیەکانت ببینە، زانیاری لەسەر بەشەکان ببینە',
+                style: TextStyle(
+                  color: ThemeColors.kGreyTextColor,
+                  fontSize: 12,
+                ),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  MyCard(
+                    imageAsset: 'assets/images/list3.svg',
+                    buttonTitle: 'ببینە',
+                    color: ThemeColors.kWhiteTextColor,
+                    text: 'کەمترین کۆنمرە',
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const KamtrinKonmra()),
+                      );
+                    },
+                  ),
+                  MyCard(
+                    imageAsset: 'assets/images/id.svg',
+                    buttonTitle: 'ببینە',
+                    color: ThemeColors.kWhiteTextColor,
+                    text: 'نمرەکانم',
+                    onTap: () {},
+                  ),
+                ],
+              ),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  MyCard(
+                    imageAsset: 'assets/images/zarabin.svg',
+                    buttonTitle: 'ببینە',
+                    color: ThemeColors.kWhiteTextColor,
+                    text: 'ڕیزبەندیەکانم',
+                  ),
+                  MyCard(
+                    imageAsset: 'assets/images/departments.svg',
+                    buttonTitle: 'ببینە',
+                    color: ThemeColors.kWhiteTextColor,
+                    text: 'بەشەکان',
+                  ),
+                ],
+              ),
+            ],
           ),
         ),
       ),
