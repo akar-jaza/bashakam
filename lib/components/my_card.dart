3405c32daab27090947d26d703d3277fe7fed63a
Akar Jaza <akarslemany08@gmail.com>
2023-07-01 17:45:56 +0300

    myCard design changed

--- lib/components/my_card.dart
+++ lib/components/my_card.dart
@@ -1,18 +1,19 @@
-
 import 'package:bashakam_barawzanko/constantes/colors.dart';
-import 'package:flutter_svg/flutter_svg.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MyCard extends StatelessWidget {
   const MyCard({
-    super.key,
+    Key? key,
     this.verticalMargin,
     required this.imageAsset,
     required this.buttonTitle,
     required this.color,
     required this.text,
     this.onTap,
-  });
+  }) : super(key: key);
+
   final double? verticalMargin;
   final String imageAsset;
   final String buttonTitle;
@@ -22,14 +23,20 @@ class MyCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final platform = Theme.of(context).platform;
+    final isIOS = platform == TargetPlatform.iOS;
+
+    double cardHeight = isIOS ? 170 : 160;
+    double cardWidth = isIOS ? 145 : 135;
+
     return InkWell(
       onTap: onTap,
       splashColor: ThemeColors.kappBarColor,
       borderRadius: BorderRadius.circular(6),
       child: Container(
         margin: const EdgeInsets.symmetric(horizontal: 7, vertical: 10),
-        height: 160,
-        width: 135,
+        height: cardHeight,
+        width: cardWidth,
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(6),
           color: ThemeColors.kMyCardColor,
@@ -39,12 +46,14 @@ class MyCard extends StatelessWidget {
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
               child: Align(
-                alignment: Alignment.centerRight, // Aligns child to the right
+                alignment: Alignment.centerRight,
                 child: Text(
                   text,
                   textAlign: TextAlign.right,
                   style: const TextStyle(
-                      color: ThemeColors.kWhiteTextColor, fontSize: 13),
+                    color: ThemeColors.kWhiteTextColor,
+                    fontSize: 13,
+                  ),
                 ),
               ),
             ),
@@ -53,15 +62,26 @@ class MyCard extends StatelessWidget {
               imageAsset,
               height: 50,
             ),
-            const SizedBox(
-              height: 10,
-            ),
-            TextButton(
-              onPressed: onTap,
-              child: Text(
-                buttonTitle,
-                style: const TextStyle(color: ThemeColors.kblueColor),
-              ),
+            const SizedBox(height: 10),
+            Container(
+              child: isIOS
+                  ? CupertinoButton(
+                      onPressed: onTap,
+                      child: Text(
+                        buttonTitle,
+                        style: const TextStyle(
+                          color: ThemeColors.kblueColor,
+                          fontFamily: 'rabarBold',
+                        ),
+                      ),
+                    )
+                  : TextButton(
+                      onPressed: onTap,
+                      child: Text(
+                        buttonTitle,
+                        style: const TextStyle(color: ThemeColors.kblueColor),
+                      ),
+                    ),
             ),
           ],
         ),
