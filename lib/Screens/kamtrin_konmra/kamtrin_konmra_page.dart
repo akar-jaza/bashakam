588d671c79d53d0d6cf89ee48ec35a2184da047f
Akar Jaza <akarslemany08@gmail.com>
2023-07-04 16:37:57 +0300

    .

--- lib/Screens/kamtrin_konmra/kamtrin_konmra_page.dart
+++ lib/Screens/kamtrin_konmra/kamtrin_konmra_page.dart
@@ -1,7 +1,5 @@
 // ignore_for_file: use_build_context_synchronously
 
-import 'dart:io';
-
 import 'package:bashakam_barawzanko/components/my_textfiled.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -97,7 +95,6 @@ class _KamtrinKonmraState extends State<KamtrinKonmra> {
       showConnectionDialog(context);
     }
   }
-
 
   @override
   Widget build(BuildContext context) {
